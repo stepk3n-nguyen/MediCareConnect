1ffc2da28f1c7a2adc52ebaf4ef0c9b19c45f6c4
Stephen Nguyen <nat0332624138@gmail.com>
2025-09-30 02:03:21 +0700

    first commit

---
+++ app/src/main/java/com/example/medicareconnect/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.medicareconnect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        // Liên kết BottomNavigationView với NavController
+        navView.setupWithNavController(navController)
+    }
+}
