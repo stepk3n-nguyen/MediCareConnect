275ad5ec8514cce33395bbc17e681b5e9e723cd2
Stephen Nguyen <nat0332624138@gmail.com>
2025-09-30 13:35:11 +0700

    fix something

--- app/src/main/java/com/example/medicareconnect/MainActivity.kt
+++ app/src/main/java/com/example/medicareconnect/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.medicareconnect
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 
@@ -12,13 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-//
-//        // Liên kết BottomNavigationView với NavController
-//        navView.setupWithNavController(navController)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
